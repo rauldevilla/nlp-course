40238e09000f2b915b088194e782995d091d5a9d
rauldevilla <raul.devilla@gmail.com>
2023-07-27 19:05:23 -0500

    Beautify code

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -5,27 +5,24 @@ from examples.transformers_example import TransformesExample
 
 class TransformersTestCase(unittest.TestCase):
 
-    def test_sentiment_analysis(self):
-        tranformer_example:TransformesExample = TransformesExample()
-        result:array = tranformer_example.sentiment_analysis(["I'm so happy", "This sucks", "I don't know how i'm feeling"])
+    tranformer_example:TransformesExample = TransformesExample()
 
-        self.assertIsNotNone(result)
-        print('-----')
+    def setUp(self):
+        print('\n-----')
+
+    def test_sentiment_analysis(self):
+        result:array = self.tranformer_example.sentiment_analysis(["I'm so happy", "This sucks", "I don't know how i'm feeling"])
         print(result)
         self.assertEqual("POSITIVE", result[0]['label'])
         self.assertEqual("NEGATIVE", result[1]['label'])
 
     def test_classification(self):
-        tranformer_example:TransformesExample = TransformesExample()
-        result:array = tranformer_example.classification("Colombia president sucks !", ["education", "politics", "business"])
-        print('-----')
+        result:array = self.tranformer_example.classification("Colombia president sucks !", ["education", "politics", "business"])
         print(result)
         self.assertGreaterEqual(0.8, result['scores'][1])
 
     def test_text_generation(self):
-        tranformer_example:TransformesExample = TransformesExample()
-        result:array = tranformer_example.text_generation("My country is")
-        print('-----')
+        result:array = self.tranformer_example.text_generation("My country is")
         print(result)
         self.assertEqual(1, len(result))
 
